384f34723c2c4478762d946578025ea0016b61c4
Kris <ssspers92@gmail.com>
2025-05-14 02:16:56 +0300

    Load environment variables from .env file
    Integrated python-dotenv to manage environment variables. Updated settings to load the SECRET_KEY from the .env file and added a .env.example template for reference. This enhances security and simplifies configuration management.

--- config/settings.py
+++ config/settings.py
@@ -9,17 +9,22 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dot_env = BASE_DIR / '.env'
+
+load_dotenv(dotenv_path=dot_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9)y&46ew@+2cwft@t0u!5-&pb5+9s!_2&6oq^f1bq&9qccog3)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
